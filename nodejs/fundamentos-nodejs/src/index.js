e1d635573d9e9ed6ae4f361e9e4f6b7f43da50cc
João Prestes <jvictor1949@hotmail.com>
2021-12-04 19:07:43 -0300

    Inclusão do nodemon

--- nodejs/fundamentos-nodejs/src/index.js
+++ nodejs/fundamentos-nodejs/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (request, response) => {
-    return response.json({mensagem: "Hello word 🐋"})
+    return response.json({mensagem: "Hello word 🐋 - Fundamentos NodeJS"})
 })
 
 app.listen(3333);

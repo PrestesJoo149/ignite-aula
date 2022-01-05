2fd559aea8b6b1ab95d38643c702ccedb6e1fd9c
João Prestes <jvictor1949@hotmail.com>
2022-01-05 16:37:39 -0300

    Concluida a Utilizando o Princípio de Responsabilidade Única (SRP)

--- nodejs/RENTALX/src/repositories/CategoriesRepository.ts
+++ nodejs/RENTALX/src/repositories/CategoriesRepository.ts
@@ -13,6 +13,7 @@ class CategoriesRepository {
     constructor(){
         this.categories = [];
     } 
+
     create({name, description}: ICreateCategoryDTO): void {
         const category  = new Category();
     
@@ -24,6 +25,16 @@ class CategoriesRepository {
      
        this.categories.push(category);
     }
+
+    list(): Category[]{
+        return this.categories;
+    }
+
+    findByName(name: string): Category {
+        const category = this.categories.find(category => category.name === name);
+        return category;
+    }
+
 }
 
 export {CategoriesRepository};
